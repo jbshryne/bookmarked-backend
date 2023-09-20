80522bc1834aa73f698359c7080bec5508b31ab7
Jon Brennan <jonbrennan@Jons-MacBook-Pro.local>
2023-09-20 19:42:26 -0400

    fixing capitalization of schema

--- models/Bookmark.js
+++ models/Bookmark.js
@@ -1,10 +1,10 @@
-const mongoose = require('../db/connection');
+const mongoose = require("../db/connection");
 
-const bookmarkSchema = new mongoose.Schema({
-    title: string,
-    url: string
+const BookmarkSchema = new mongoose.Schema({
+  title: string,
+  url: string,
 });
- 
-const Bookmark= mongoose.model("Bookmark", bookmarkSchema);
 
-module.exports = Bookmark
+const Bookmark = mongoose.model("Bookmark", BookmarkSchema);
+
+module.exports = Bookmark;
