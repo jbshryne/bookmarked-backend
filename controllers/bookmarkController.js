52348fa0bcce27f6e2f37179ca3dfeb8006da794
Jon Brennan <jonbrennan@Jons-MacBook-Pro.local>
2023-09-20 20:49:57 -0400

    adding index & create routes

--- controllers/bookmarkController.js
+++ controllers/bookmarkController.js
@@ -2,23 +2,41 @@ const express = require("express");
 const router = express.Router();
 const Bookmark = require("../models/Bookmark");
 
+//Index
+router.get("/", async (req, res) => {
+  try {
+    res.json(await Bookmark.find());
+  } catch (error) {
+    res.status(400).json(error);
+  }
+});
 
+//Create
+router.get("/", async (req, res) => {
+  try {
+    res.json(await Bookmark.create(req.body));
+  } catch (error) {
+    res.status(400).json(error);
+  }
+});
 
 //Delete
-router.delete("/:id", async (req,res) => {
-    try{
-        res.json(await Bookmark.findByIdAndDelete(req.params.id))
-     }catch(error){
-        res.status(400).json(error)
-     }
+router.delete("/:id", async (req, res) => {
+  try {
+    res.json(await Bookmark.findByIdAndDelete(req.params.id));
+  } catch (error) {
+    res.status(400).json(error);
+  }
 });
 
 //Update
-router.put('/:id', async (req,res) => {
-    try{
-        res.json(await Bookmark.findByIdAndUpdate(req.params.id, req.body, { new: true}))
-     }catch(error){
-        res.status(400).json(error)
-     }
+router.put("/:id", async (req, res) => {
+  try {
+    res.json(
+      await Bookmark.findByIdAndUpdate(req.params.id, req.body, { new: true })
+    );
+  } catch (error) {
+    res.status(400).json(error);
+  }
 });
 module.exports = router;
